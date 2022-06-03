e2eecf423110853079960234cd3cb1974327f604
Kadin Pegram <kpegra1@students.towson.edu>
2022-06-03 17:00:11 -0400

    initial

---
+++ LA.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class LA 
+{
+    public static void main(String[] args) 
+    {
+        {
+            Scanner input = new Scanner(System.in);
+            String FileName;
+
+            System.out.println("Type the file name: ");
+            FileName = input.nextLine();
+
+            System.out.println("You inputted file -> " + FileName);
+        }
+    }
+}
---
+++ Tokenizer.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+
+public class Tokenizer {
+    private BufferedReader reader;
+    public Tokenizer (BufferedReader reader){
+        this.reader = reader;
+    }
+    
+}
